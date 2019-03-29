d1c72850785803e9a97e6952d71396121555ddbb
Akhil <akhil9tiet@gmail.com>
2019-03-29 13:39:45 -0700

    build basic empty page

--- webapp/src/components/layout.js
+++ webapp/src/components/layout.js
@@ -30,24 +30,29 @@ const Layout = ({ children }) => (
     render={data => (
       <Provider theme={theme}>
         <Grid templateRows="120px 1fr 20px" style={{ minHeight: "700px" }}>
-          <>
+          <Grid.Item>
             <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-              style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `0px 1.0875rem 1.45rem`,
-                paddingTop: 0,
-              }}
-            >
-              <main>{children}</main>
-              <footer>
-                © {new Date().getFullYear()}, Built with
-                {` `}
-                <a href="https://www.gatsbyjs.org">Gatsby</a>
-              </footer>
-            </div>
-          </>
+          </Grid.Item>
+          <Grid.Item>
+            <CenterColumn as="main">
+              <div
+                style={{
+                  margin: `0 auto`,
+                  maxWidth: 960,
+                  padding: `0px 1.0875rem 1.45rem`,
+                  paddingTop: 0,
+                }}
+              >
+                <main>{children}</main>
+              </div>
+            </CenterColumn>
+          </Grid.Item>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+          <Grid.Item />
         </Grid>
       </Provider>
     )}
