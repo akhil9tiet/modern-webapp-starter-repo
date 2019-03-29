44ae0c33fffaa618980dfe8344dfc404a25a8b9c
Akhil <akhil9tiet@gmail.com>
2019-03-29 13:17:24 -0700

    Adding random config

--- webapp/gatsby-config.js
+++ webapp/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Shred grocery list`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    randomData: "Information not needed",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
