dc1a283fc5bdede1362d00b4a81c0c73375646a4
Akhil <akhil9tiet@gmail.com>
2019-03-29 13:33:32 -0700

    Added layout

--- webapp/src/components/layout.js
+++ webapp/src/components/layout.js
@@ -10,7 +10,11 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import { CenterColumn, theme } from "../styles"
+import { Provider, Grid } from "reakit"
+
 import "./layout.css"
+import { styles } from "ansi-colors"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,24 +28,28 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <Provider theme={theme}>
+        <Grid templateRows="120px 1fr 20px" style={{ minHeight: "700px" }}>
+          <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                paddingTop: 0,
+              }}
+            >
+              <main>{children}</main>
+              <footer>
+                © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+              </footer>
+            </div>
+          </>
+        </Grid>
+      </Provider>
     )}
   />
 )
